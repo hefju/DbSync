e567829eef80ec548d311ab9d2db378f259a71a5
hefju <hefju@qq.com>
2016-09-01 22:24:28 +0800

    更新多个表

--- TSAuto/synctable.go
+++ TSAuto/synctable.go
@@ -22,7 +22,8 @@ func init() {
 	if err != nil {
 		log.Println(err)
 	}
-	err = engine.Sync2(new(Coordinate),new(PageProfile))
+	err = engine.Sync2(new(Coordinate), new(PageProfile),
+		new(Microplate), new(MicroplateInfo), new(SystemImaging), new(IndependentImaging))
 	if err != nil {
 		log.Println(err)
 	}
@@ -46,38 +47,80 @@ func Insert() int64 {
 }
 
 type Coordinate struct {
-	ID                int64 //`xorm:pk,autoincr`
-	CoordinateID     int
-	CustomID	string
-	FormName string
-	ItemType string
+	ID              int64 //`xorm:pk,autoincr`
+	CoordinateID    int
+	CustomID        string
+	FormName        string
+	ItemType        string
 	CoordinateOrder int
-	AreaName	string
+	AreaName        string
 	CoordinateName  string
 	CoordinateDesc  string
-	XAxis             float64
-	YAxis             float64
-	ZAxis             float64
-	AAxis             float64
-	BAxis             float64
-	CAxis             float64
-	IsZero            int
-	Settled           int
-	UpdatedAt time.Time `xorm:"updated"`
-	CreateAt          time.Time `xorm:"created"`
+	XAxis           float64
+	YAxis           float64
+	ZAxis           float64
+	AAxis           float64
+	BAxis           float64
+	CAxis           float64
+	IsZero          int
+	Settled         int
+	UpdatedAt       time.Time `xorm:"updated"`
+	CreateAt        time.Time `xorm:"created"`
 }
 type PageProfile struct {
 	ID          int64
-	CustomID	string
+	CustomID    string
 	FormName    string
 	ItemType    string
 	ItemOrder   int
-	AreaName	string
+	AreaName    string
 	ItemNumber  string
 	ItemName_CN string
-	Display_CN string
+	Display_CN  string
 	ItemName_EN string
-	Display_EN string
-	UpdatedAt time.Time `xorm:"updated"`
-	CreateAt          time.Time `xorm:"created"`
+	Display_EN  string
+	UpdatedAt   time.Time `xorm:"updated"`
+	CreateAt    time.Time `xorm:"created"`
+}
+
+type Microplate struct {
+	ID             int64
+	MicroplateName string
+}
+
+type MicroplateInfo struct {
+	ID           int64
+	MicroplateID int
+	Manufacturer string
+	Numwell      int
+	Rows         int
+	Cols         int
+	Height       float32
+	Width        float32
+	Margin_x     float32
+	Margin_y     float32
+	Pitch_x      float32
+	Pitch_y      float32
+	Radius       float32
+	Description  string
+}
+type SystemImaging struct {
+	ID                int64
+	SourcePlateName   string
+	SourceMicroplates string
+	Incubators        string
+	Project           string
+	StartTime         string
+	EndTime           string
+}
+
+type IndependentImaging struct {
+	ID                int64
+	Name              string
+	SourceMicroplates string
+	//    Incubators string
+	//    Project string
+	StartTime string
+	EndTime   string
+	User      string
 }
